2e938ee391df78548c8b6182d0c79604bddb5ad3
MurtazaKhannn <murtazakhan1910@gmail.com>
2024-11-09 16:41:00 +0530

    multiple files

--- src/components/ui/file-upload.tsx
+++ src/components/ui/file-upload.tsx
@@ -11,6 +11,7 @@ import QRCode from 'qrcode';
 import { Flip, toast } from "react-toastify";
 import CryptoJS from 'crypto-js';
 import { TextGenerateEffect } from "./text-generate-effect";
+import JSZip from 'jszip';  // Import JSZip
 
 
 const mainVariant = {
@@ -46,8 +47,12 @@ export const FileUpload = () => {
     const [files, setFiles] = useState<File[]>([]);
     const fileInputRef = useRef<HTMLInputElement>(null);
     const fileRef = useRef<HTMLDivElement>(null);
-    const [qrCode, setQrCode] = useState('');
-    const [fileLink, setFileLink] = useState('');
+    const [qrCodes, setQrCodes] = useState<{ [key: string]: string }>({});
+    const [fileLinks, setFileLinks] = useState<{ [key: string]: string }>({});
+    const [qrCode, setQrCode] = useState<string>();
+    const [fileLink, setFileLink] = useState<string>();
+
+
 
     const handleClick = () => {
         fileInputRef.current?.click();
@@ -69,67 +74,97 @@ export const FileUpload = () => {
             return;
         }
 
-
         setFiles((prevFiles) => [...prevFiles, ...newFiles]);
 
-        for (const file of newFiles) {
-            const timestamp = Date.now();
-            const uniqueFileName = `public/${timestamp}_${file.name}`
+        let fileToUpload;
 
-            const encryptedBlob = await file.arrayBuffer().then((buffer) => {
+        if (validFiles.length > 1) {
+            const zip = new JSZip();
+
+            validFiles.forEach(file => {
+                zip.file(file.name, file);
+            });
+
+            const zipBlob = await zip.generateAsync({ type: 'blob' });
+
+            fileToUpload = await zipBlob.arrayBuffer().then((buffer) => {
+                const encryptedData = CryptoJS.AES.encrypt(CryptoJS.lib.WordArray.create(buffer), process.env.NEXT_PUBLIC_ENCRYPTION_KEY!);
+                return new Blob([encryptedData.toString()], { type: 'application/zip' });
+            });
+        } else {
+            const file = validFiles[0];
+            fileToUpload = await file.arrayBuffer().then((buffer) => {
                 const encryptedData = CryptoJS.AES.encrypt(CryptoJS.lib.WordArray.create(buffer), process.env.NEXT_PUBLIC_ENCRYPTION_KEY!);
                 return new Blob([encryptedData.toString()], { type: file.type });
             });
-
-            const { data, error: uploadError } = await supabase.storage
-                .from('uploads')
-                .upload(`${uniqueFileName}`, encryptedBlob);
-
-            if (uploadError) {
-                console.error('Error uploading file:', uploadError.message);
-                continue;
-            }
-
-            console.log('File Uploaded Successfully', data);
-            const uniqueID = generateUniqueId();
-
-            const fileUrl = `${process.env.NEXT_PUBLIC_SERVER_URL}file/${uniqueID}`;
-
-            const { error: dbError } = await supabase
-                .from('uploads')
-                .insert([
-                    {
-                        id: uniqueID,
-                        file_name: file.name,
-                        file_size: file.size,
-                        file_type: file.type,
-                        file_path: `/storage/v1/object/public/uploads/${uniqueFileName}`,
-                        created_at: new Date().toISOString(),
-                    },
-                ]);
-
-            if (dbError) {
-                console.error('Error storing file metadata:', dbError.message);
-            } else {
-                console.log('File metadata stored successfully:', { name: file.name, url: fileUrl });
-            }
-
-            await generateQrCode(fileUrl);
-            console.log("qr code generated");
         }
+
+        const timestamp = Date.now();
+        const uniqueFileName = validFiles.length > 1 ? `public/${timestamp}_files.zip` : `public/${timestamp}_${validFiles[0].name}`;
+
+        const { data, error: uploadError } = await supabase.storage
+            .from('uploads')
+            .upload(uniqueFileName, fileToUpload);
+
+        if (uploadError) {
+            console.error('Error uploading file:', uploadError.message);
+            return;
+        }
+
+        console.log('File Uploaded Successfully', data);
+        const uniqueID = generateUniqueId();
+
+        const fileUrl = `${process.env.NEXT_PUBLIC_LOCAL_URL}file/${uniqueID}`;
+        setFileLink(fileUrl);
+
+        const { error: dbError } = await supabase
+            .from('uploads')
+            .insert([{
+                id: uniqueID,
+                file_name: validFiles.length > 1 ? 'files.zip' : validFiles[0].name,
+                file_size: fileToUpload.size,
+                file_type: validFiles.length > 1 ? 'application/zip' : validFiles[0].type,
+                file_path: `/storage/v1/object/public/uploads/${uniqueFileName}`,
+                created_at: new Date().toISOString(),
+            }]);
+
+        if (dbError) {
+            console.error('Error storing file metadata:', dbError.message);
+        } else {
+            console.log('File metadata stored successfully:', { name: validFiles.length > 1 ? 'files.zip' : validFiles[0].name, url: fileUrl });
+        }
+
+        await generateQrCode(fileUrl);
+
+        setFileLinks((prevLinks) => ({ ...prevLinks, [uniqueFileName]: fileUrl }));
     };
+
+
+    // const generateQrCode = async (fileName: string) => {
+    //     const url = fileLinks[fileName];
+    //     if (url) {
+    //         try {
+    //             const qrCodeDataUrl = await QRCode.toDataURL(url);
+    //             setQrCodes((prevCodes) => ({ ...prevCodes, [fileName]: qrCodeDataUrl }));
+    //         } catch (error) {
+    //             console.error("Error generating QR code", error);
+    //         }
+    //     }
+    // };
+
 
     const generateQrCode = async (url: string) => {
         try {
-            const qrCodeDataUrl = await QRCode.toDataURL(url);
-            setQrCode(qrCodeDataUrl);
-            setFileLink(url);
+            const qrCodeDataUrl = await QRCode.toDataURL(url); // Generate QR code for combined links
+            setQrCode(qrCodeDataUrl);  // Set QR code state
+            setFileLink(url);  // Store the combined file links
             fileInputRef?.current?.setAttribute("disabled", "true");
-
         } catch (error) {
             console.error("Error generating QR code", error);
         }
     };
+
+
 
 
     function generateUniqueId() {
@@ -161,7 +196,7 @@ export const FileUpload = () => {
     }, []);
 
     const { getRootProps, isDragActive } = useDropzone({
-        multiple: false,
+        multiple: true,
         noClick: true,
         onDrop: handleFileChange,
         onDropRejected: (error) => {
@@ -172,14 +207,13 @@ export const FileUpload = () => {
     return (
         <div className="w-full py-12" {...getRootProps()}>
             <motion.div
-                onClick={handleClick}
-                whileHover="animate"
-                className="p-10 max-sm:p-5 group/file block rounded-lg cursor-pointer w-full relative overflow-hidden"
+                className="p-10 max-sm:p-5 group/file block rounded-lg w-full relative overflow-hidden"
             >
                 <input
                     ref={fileInputRef}
                     id="file-upload-handle"
                     type="file"
+                    multiple
                     onChange={(e) => {
                         const files = Array.from(e.target.files || []);
                         console.log("File selected:", files);
@@ -201,16 +235,21 @@ export const FileUpload = () => {
                                 <TextGenerateEffect className='text-white sm:hidden' words={title} />
                             </span>
                         </div>
-                        <div className="relative w-full mt-10 max-w-xl mx-auto">
+                        <motion.div className="relative w-full mt-10 max-w-xl mx-auto cursor-pointer overflow-x-hidden h-[40vh] overflow-y-auto"
+                            whileHover="animate"
+
+                            onClick={handleClick}
+                        >
+
                             {files.length > 0 &&
                                 files.map((file, idx) => (
                                     <motion.div
                                         key={"file" + idx}
                                         layoutId={idx === 0 ? "file-upload" : "file-upload-" + idx}
-                                        className={cn(
-                                            "relative overflow-hidden z-40 bg-white dark:bg-neutral-900 flex flex-col items-start justify-start md:h-24 p-4 mt-4 w-full mx-auto rounded-md",
-                                            "shadow-sm"
-                                        )}
+                                        // className={cn(
+                                        //     "relative overflow-hidden z-40 bg-white dark:bg-neutral-900 flex flex-col items-start justify-start md:h-24 p-4 mt-4 w-full mx-auto rounded-md",
+                                        //     "shadow-sm"
+                                        // )}
                                     >
                                         <div className="flex justify-between w-full items-center gap-4">
                                             <motion.p
@@ -287,63 +326,98 @@ export const FileUpload = () => {
                                     className="absolute opacity-0 border border-dashed border-sky-400 inset-0 z-30 bg-transparent flex items-center justify-center h-32 mt-4 w-full max-w-[8rem] mx-auto rounded-md"
                                 ></motion.div>
                             )}
-                        </div>
+                        </motion.div>
 
 
 
                     </div>
 
-                    {qrCode && <div className="p-5 bg-black z-[1000] max-sm:pb-52">
-                        <img draggable={false} className="rounded-xl mb-5 m-auto select-none text-white" src={qrCode} alt="QR Code" />
-                        <span className="flex gap-2 bg-black justify-center">
-                            <motion.button
-                                className=" bg-slate-800 max-sm:w-[95%] no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-px text-xs font-semibold leading-6 text-white inline-block"
-                                whileHover={{ scale: 1.05 }}
-                                whileTap={{ scale: 0.95 }}
-                                onClick={() => {
-                                    navigator.clipboard.writeText(fileLink).then(() => {
-                                        toast.success('Copied ✔', {
-                                            position: "bottom-left",
-                                            autoClose: 2000,
-                                            hideProgressBar: false,
-                                            closeOnClick: true,
-                                            pauseOnHover: true,
-                                            draggable: true,
-                                            progress: 0,
-                                            theme: "light",
-                                            transition: Flip,
-                                        });
-                                    })
-                                }}
-                            >
-                                <span className="absolute inset-0 overflow-hidden rounded-full">
-                                    <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100"></span>
-                                </span>
-                                <div className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10 ">
-                                    <span>{`Copy`}</span>
-                                    <svg
-                                        width="16"
-                                        height="16"
-                                        viewBox="0 0 24 24"
-                                        fill="none"
-                                        xmlns="http://www.w3.org/2000/svg"
-                                    >
-                                        <path
-                                            stroke="currentColor"
-                                            strokeLinecap="round"
-                                            strokeLinejoin="round"
-                                            strokeWidth="1.5"
-                                            d="M10.75 8.75L14.25 12L10.75 15.25"
-                                        ></path>
-                                    </svg>
-                                    <a href={fileLink} target="_blank" className="font-kanit px-5 p-1 rounded-full">{fileLink}</a>
+                    {/* <div className="h-[40vh] overflow-y-auto">
+                <h3>Selected Files</h3>
+                <ul>
+                    {files.map((file, index) => (
+                        <li key={index}>
+                            {file.name}
+                            <button onClick={() => generateQrCode(file.name)}>Generate QR</button>
 
+                            {qrCodes[file.name] && (
+                                <div>
+                                    <img src={qrCodes[file.name]} alt="QR Code" />
+                                    <a href={fileLinks[file.name]} target="_blank" rel="noopener noreferrer">
+                                        Download {file.name}
+                                    </a>
                                 </div>
-                                <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40"></span>
+                            )}
+                        </li>
+                    ))}
+                </ul>
+            </div> */}
 
-                            </motion.button>
-                        </span>
-                    </div>}
+                    <div className="h-[40vh] overflow-y-auto">
+                        {/* <h3>Selected Files</h3> */}
+                        <ul>
+                            {/* {files.map((file, index) => (
+                        <li key={index}>
+                            {file.name} */}
+                            {/* Optionally, provide individual links for each file */}
+                            {/* <a href={fileLinks[file.name]} target="_blank" rel="noopener noreferrer">
+                                Download {file.name}
+                            </a>
+                        </li>
+                    ))} */}
+                        </ul>
+                    </div>
+
+                    {/* Button to generate one QR code for all files */}
+                    {/* <button onClick={generateQrCode}>Generate QR for All Files</button> */}
+
+
+                    <div className="flex flex-col gap-2 items-center justify-center">
+                        {qrCode && (
+                            <div className="flex flex-col items-center justify-center gap-2">
+                                <img className="rounded-md" src={qrCode} alt="QR Code" />
+                                <p>Scan the QR code to access all files.</p>
+                            </div>
+
+                        )}
+
+                        {fileLink && (
+                            <div className="download-link flex gap-2">
+                                <span className="cursor-pointer text-xs flex justify-center items-center bg-white px-3 py-1 text-black rounded-md font-bold" onClick={() => {
+                                    navigator.clipboard.writeText("Your text here");
+                                    alert("Text copied to clipboard!");
+                                }}>Copy</span>
+                                <a href={fileLink} className="download-button">
+                                    <button className="bg-slate-800 no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-md p-px text-xs font-semibold leading-6  text-white inline-block">
+                                        <span className="absolute inset-0 overflow-hidden rounded-md">
+                                            <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100"></span>
+                                        </span>
+                                        <div className="relative flex space-x-2 items-center z-10 rounded-md bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10 ">
+                                            <span>{fileLink}</span>
+                                            <svg
+                                                width="16"
+                                                height="16"
+                                                viewBox="0 0 24 24"
+                                                fill="none"
+                                                xmlns="http://www.w3.org/2000/svg"
+                                            >
+                                                <path
+                                                    stroke="currentColor"
+                                                    strokeLinecap="round"
+                                                    strokeLinejoin="round"
+                                                    strokeWidth="1.5"
+                                                    d="M10.75 8.75L14.25 12L10.75 15.25"
+                                                ></path>
+                                            </svg>
+                                        </div>
+                                        <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40"></span>
+                                    </button>
+                                </a>
+                            </div>
+                        )}
+
+                    </div>
+
 
 
                 </div>
